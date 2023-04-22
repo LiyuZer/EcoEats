804ce2a2039781e3461cdd6a6d1eb851ee86a705
Eric <eric.dianhao.wu@gmail.com>
2023-04-22 01:32:17 -0700

    make prettier

--- eco_eats/src/Webcam.js
+++ eco_eats/src/Webcam.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Webcam from "react-webcam";
+import { Button, Box } from '@mui/material';
 
 
 const videoConstraints = {
@@ -9,32 +10,53 @@ const videoConstraints = {
 };
 
 const WebcamCapture = () => {
+  const [isCaptureMode, setIsCaptureMode] = React.useState(true);
+
   const [imgSrc, setImgSrc] = React.useState(null);
 
   return (
   <>
-    <Webcam
-      audio={false}
-      height={720}
-      screenshotFormat="image/jpeg"
-      width={1280}
-      videoConstraints={videoConstraints}
-    >
-      {({ getScreenshot }) => (
-        <button
-          onClick={() => {
-            setImgSrc(getScreenshot());
-          }}
-        >
-          Capture photo
-        </button>
-      )}
-    </Webcam>
-    {imgSrc && (
-      <img
-        src={imgSrc}
-      />
-    )}
+    {isCaptureMode && 
+      <Webcam
+        audio={false}
+        screenshotFormat="image/jpeg"
+        width={280}
+        videoConstraints={videoConstraints}
+      >
+        {({ getScreenshot }) => (
+          <Box m={1}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                setImgSrc(getScreenshot());
+                setIsCaptureMode(!isCaptureMode);
+              }}
+            >
+              Capture photo
+            </Button>
+          </Box>
+        )}
+      </Webcam>
+    }
+    {!isCaptureMode && (
+      <>
+        <img
+          src={imgSrc}
+          width={280}
+        />
+        <Box m={1}>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setIsCaptureMode(!isCaptureMode);
+            }}
+          >
+            Retake
+          </Button>
+        </Box>
+      </>
+      )
+    }
   </>);
 
 };
