933424c20108dd2e3c2ea1ad60c1e6e7bdc307c3
Eric <eric.dianhao.wu@gmail.com>
2023-04-22 11:01:43 -0700

    add a build script for client (apache)

---
+++ eco_eats/run_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# export PUBLIC_URL=https://tslyriccompletion.com
+time npm run build
+cd build
+cp -r * /var/www/html
+
+echo "Successfully built the project and copied files to /var/www/html. Changes should now be live."
+
